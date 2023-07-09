06c34d33f7c3bebe9fb8e3e6dbb87a9a827bdcae
Valentin Schnabl <valentin.cello@gmail.com>
2023-07-09 17:53:47 +0200

    add google tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,6 +16,19 @@ export default function RootLayout({
 }) {
   return (
     <html lang="de" className='scroll-smooth'>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-NQ6JBNL8JY"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'GA_MEASUREMENT_ID');
+        `}
+      </Script>
       <body className={inter.className}>{children}</body>
     </html>
   )
